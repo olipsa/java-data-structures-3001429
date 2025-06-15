788d52f49ed3f8a57d9ee94a65fc8bc371379665
Ondina Lipsa <ondina_patricia@yahoo.com>
2025-06-15 20:30:05 +0300

    Linked Lists section

--- src/main/java/LinkedListExamples.java
+++ src/main/java/LinkedListExamples.java
@@ -7,20 +7,24 @@ public class LinkedListExamples {
   public static void main(String[] args) {
 
     LinkedList<String> shoppingList = new LinkedList<>();
-    shoppingList.add("apple");
-    shoppingList.add("banana");
-    shoppingList.add("pear");
+    shoppingList.add("Apple");
+    shoppingList.add("Banana");
+    shoppingList.add("Pear");
     System.out.println(shoppingList);
 
-    shoppingList.add(1, "mango");
+    shoppingList.add(1, "Mango"); // O(n)
+    System.out.println(shoppingList);
+    // Linked lists are quicker for adding and deleting elements
+    // Array lists are quicker for randomly accessing elements -> O(1)
+
+    // Linked lists are slower for randomly accessing elements -> O(n)
+    // Array lists are slower for adding and deleting elements -> O(n)
+
+    shoppingList.removeFirst(); // O(1)
     System.out.println(shoppingList);
 
-    shoppingList.removeFirst();
-    System.out.println(shoppingList);
-
-    List<String> synchronizedShoppingList = Collections.synchronizedList(shoppingList);
+    List<String> synchronizedShoppingList = Collections.synchronizedList(shoppingList); // copy of the shopping list, but thread safe
     System.out.println(synchronizedShoppingList);
-    
   }
 
 }
