0ded091a85da808d04626748a9f489be12aa1c10
Ondina Lipsa <ondina_patricia@yahoo.com>
2025-06-15 21:32:47 +0300

    Queue and PriorityQueue chapter

--- src/main/java/QueueExamples.java
+++ src/main/java/QueueExamples.java
@@ -1,5 +1,4 @@
 import java.util.ArrayDeque;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -7,15 +6,26 @@ public class QueueExamples {
 
   public static void main(String[] args) {
 
-    Queue<String> queue = new ArrayDeque<>();
-    queue.offer("Person One");
-    queue.offer("Person Two");
-    queue.offer("Person Three");
+    Queue<String> queue = new ArrayDeque<>(); // could also use LinkedList, but ArrayDeque has better performance
+    Queue<String> priorityQueue = new PriorityQueue<>(); // The priority is the alphabetical order of elements
+
+    // Adds elements at the end of the array (queue)
+    queue.offer("Person1");
+    priorityQueue.offer("Person One");
+    queue.offer("Person2");
+    priorityQueue.offer("Person Two");
+    queue.offer("Person3");
+    priorityQueue.offer("Person Three");
     System.out.println(queue);
-    System.out.println(queue.peek());
-    queue.poll();
+    System.out.println(priorityQueue);
+
+    System.out.println(queue.peek()); // returns the 1st element (the head of the queue)
+
+    queue.poll(); // removes 1st element
+    priorityQueue.poll(); // removes 1st element and processes elements based on priority/alphabetical order
     System.out.println(queue);
-    
+    System.out.println(priorityQueue);
+
   }
 
 }
